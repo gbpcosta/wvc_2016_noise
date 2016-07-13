65d786e2a1a1b957594bbc11b5edf5fb3e53bc5e
Welinton Andrey <welintonandrey@gmail.com>
2016-07-13 10:43:55 -0300

    add salt&pepper noise

--- noise/addNoise.m
+++ noise/addNoise.m
@@ -4,7 +4,7 @@ function addNoise(pathImages, extImages, noiseType, noiseLevel)
     di = dir(pathImages);
     folders = {};
     aux = 1;
-    
+
     %Get all subfolders
     for i=1:length(di)
         if (di(i).isdir == 1 && (strcmp(di(i).name,'.') == 0 && strcmp(di(i).name,'..') == 0))
@@ -12,38 +12,38 @@ function addNoise(pathImages, extImages, noiseType, noiseLevel)
             aux = aux + 1;
         end
     end
-    
+
     %Get new pah
     cd(pathImages);
     cd('..');
     newPath = pwd();
     newPath = strcat(newPath,'/',noiseType,'-',num2str(noiseLevel));
-    
+
     %Check if exist
     if ~exist(newPath, 'dir')
         mkdir(newPath);
     end
-    
+
     %Enter in new path
     cd(newPath);
-    
+
     %Create subfolders and add noise
     for i=1:length(folders)
         newSubPath = strcat(newPath,'/',folders{i});
-        
+
         %Check if exist
         if ~exist(newSubPath, 'dir')
             mkdir(newSubPath);
         end
-        
+
         %Read original images
         images = dir(strcat(pathImages,'/',folders{i},'/','*.',extImages));
         imgVec = {};
-        
+
         for j=1:length(images)
             imgVec{j} = imread(strcat(pathImages,'/',folders{i},'/',images(j).name));
         end
-        
+
         %Add noise in images
         if strcmp(noiseType,'gaussian') == 1
             for j=1:length(imgVec)
@@ -63,12 +63,19 @@ function addNoise(pathImages, extImages, noiseType, noiseLevel)
                 disp(str)
                 imwrite(noise,str);
             end
+        elseif strcmp(noiseType,'sp') == 1
+            for j=1:length(imgVec)
+                %Salt and Pepper noise
+                noise = imnoise(imgVec{j},'salt & pepper',noiseLevel)
+                str = strcat(newSubPath,'/',images(j).name);
+                disp(str)
+                imwrite(noise,str);
+            end
         end
 
         clear images imgVec
-        
+
     end
-    
-    
+
     cd(pathRet);
 end
