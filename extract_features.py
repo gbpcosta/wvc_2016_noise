7fd515719ad31d2438edff9eae75b38a47176e73
Gabriel de Barros Paranhos da Costa <gabrielbpcosta@gmail.com>
2016-07-26 10:04:09 -0300

    Remove print descriptor image and DAISY descriptor

--- extract_features.py
+++ extract_features.py
@@ -9,75 +9,6 @@ from skimage import io, color, exposure
 from scipy import misc
 import matplotlib.pyplot as plt
 
-def print_hog_images(img_gray, hog_img, filename="hog.png"):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
-
-    ax1.axis('off')
-    ax1.imshow(img_gray, cmap=plt.cm.gray)
-    ax1.set_title('Input image')
-    ax1.set_adjustable('box-forced')
-
-    # Rescale histogram for better display
-    hog_img_rescaled = exposure.rescale_intensity(hog_img, in_range=(0, 0.02))
-
-    ax2.axis('off')
-    ax2.imshow(hog_img_rescaled, cmap=plt.cm.gray)
-    ax2.set_title('Histogram of Oriented Gradients')
-    ax1.set_adjustable('box-forced')
-    plt.savefig(filename)
-
-def overlay_labels(img, lbp, labels):
-    mask = np.logical_or.reduce([lbp == each for each in labels])
-    return label2rgb(mask, image=img, bg_label=0, alpha=0.5)
-
-def highlight_bars(bars, indexes):
-    for i in indexes:
-        bars[i].set_facecolor('r')
-
-def hist(ax, lbp):
-    n_bins = lbp.max() + 1
-    return ax.hist(lbp.ravel(), normed=True, bins=n_bins, range=(0, n_bins),
-                   facecolor='0.5')
-
-def print_lbp_image(img, lbp, filename="lbp.png"):
-    fig, (ax_img, ax_hist) = plt.subplots(nrows=2, ncols=3, figsize=(9, 6))
-    plt.gray()
-
-    titles = ('edge', 'flat', 'corner')
-    w = width = radius - 1
-    edge_labels = range(n_points // 2 - w, n_points // 2 + w + 1)
-    flat_labels = list(range(0, w + 1)) + list(range(n_points - w, n_points + 2))
-    i_14 = n_points // 4            # 1/4th of the histogram
-    i_34 = 3 * (n_points // 4)      # 3/4th of the histogram
-    corner_labels = (list(range(i_14 - w, i_14 + w + 1)) +
-                     list(range(i_34 - w, i_34 + w + 1)))
-
-    label_sets = (edge_labels, flat_labels, corner_labels)
-
-    for ax, labels in zip(ax_img, label_sets):
-        ax.imshow(overlay_labels(img, lbp, labels))
-
-    for ax, labels, name in zip(ax_hist, label_sets, titles):
-        counts, _, bars = hist(ax, lbp)
-        highlight_bars(bars, labels)
-        ax.set_ylim(ymax=np.max(counts[:-1]))
-        ax.set_xlim(xmax=n_points + 2)
-        ax.set_title(name)
-
-    ax_hist[0].set_ylabel('Percentage')
-    for ax in ax_img:
-        ax.axis('off')
-
-    plt.savefig(filename)
-
-def print_daisy_image(daisy_img, daisy_descs, filename="daisy.png"):
-    fig, ax = plt.subplots()
-    ax.axis('off')
-    ax.imshow(daisy_img)
-    descs_num = daisy_descs.shape[0] * daisy_descs.shape[1]
-    ax.set_title('%i DAISY descriptors extracted:' % descs_num)
-    plt.savefig(filename)
-
 def find_smallest_shape(dataset, extensionsToCheck):
     img_new_shape = [float("inf"), float("inf")]
     classes = next(os.walk(dataset))[1]
@@ -135,12 +66,9 @@ def extract_features():
                 else:
                     img_gray = resize(img_gray, [new_shape[1], new_shape[0]])
 
-                fd, hod_img = hog(img_gray, orientations=8, pixels_per_cell=(16, 16),                              cells_per_block=(1, 1), visualise=True)
+                fd, hog_img = hog(img_gray, orientations=8, pixels_per_cell=(16, 16),                              cells_per_block=(1, 1), visualise=True)
 
                 fv_matrix = np.vstack([fv_matrix, fd]) if fv_matrix.size else fd
-
-                if(args.save_image == True):
-                    print_hog_images(img_gray, hog_img, filename="hog.png")
 
             elif(args.feature_extraction == "LBP"):
                 img_gray = color.rgb2gray(img)
@@ -155,21 +83,6 @@ def extract_features():
 
                 fv_matrix = np.vstack([fv_matrix, lbp_hist]) if fv_matrix.size else lbp_hist
 
-                if(args.save_image == True):
-                    print_lbp_image(img_gray, lbp, filename="lbp.png")
-
-            elif(args.feature_extraction == "DAISY"):
-                # TODO: Fix bugs! Problems with the selected parameter values
-                img_gray = color.rgb2gray(img)
-
-                descs, descs_img = daisy(img_gray, step=180, radius=58, rings=2, histograms=6, orientations=8, visualize=True)
-
-                fv_matrix = np.vstack([fv_matrix, descs]) if fv_matrix.size else descs
-
-                if(args.save_image == True):
-                    print_lbp_image(img_gray, descs, filename="daisy.png")
-
-            # elif(args.feature_extraction == "ORB"):
             elif(args.feature_extraction == "SIFT"):
                 img_gray = color.rgb2gray(img)
                 img_gray = np.array(img_gray)
